e190d2979cb23a1f3bfcc9689befe02b64e4c025
Luis Arias <lugearma@gmail.com>
2017-11-30 10:29:07 -0600

    Initial Commit

---
+++ ActivityPT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityPT
+//
+//  Created by Luis Arias on 11/30/17.
+//  Copyright © 2017 Terco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
