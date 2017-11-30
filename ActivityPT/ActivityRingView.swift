42903a374a2a79b6a75dfba9157e29f4b0e08587
Luis Arias <lugearma@gmail.com>
2017-11-30 13:52:18 -0600

    First commit

---
+++ ActivityPT/ActivityRingView.swift
@@ -0,0 +1,9 @@
+//
+//  ActivityRingView.swift
+//  ActivityPT
+//
+//  Created by Luis Arias on 11/30/17.
+//  Copyright © 2017 Terco. All rights reserved.
+//
+
+import Foundation
---
+++ ActivityPT/WorkoutSummaryView.swift
@@ -0,0 +1,9 @@
+//
+//  WorkoutSummaryView.swift
+//  ActivityPT
+//
+//  Created by Luis Arias on 11/30/17.
+//  Copyright © 2017 Terco. All rights reserved.
+//
+
+import Foundation
